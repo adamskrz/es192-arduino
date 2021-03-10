cd8f4c4734955554cab5af75b0f5ccc012d4add4
Adam Skrzymowski <35405850+adamskrz@users.noreply.github.com>
2021-03-10 17:01:35 +0000

    Removed dummy code

--- code.ino
+++ code.ino
@@ -77,9 +77,4 @@ void loop()
     digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
     delay(delay_time);
   }
-
-  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);                     // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-  delay(1000);                     // wait for a second
 }

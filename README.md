b187dd7fa80baa6f0e8bf831dd1288b8593bd264
Adam Skrzymowski <35405850+adamskrz@users.noreply.github.com>
2021-03-10 16:51:46 +0000

    Added some code for controlling LED blink speed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## ES192 Project Arduino code
+# ES192 Project Arduino code
 
 This repo contains Arduino code for my Electronics Sprint Porject for ES192 Engineering Design
--- code.ino
+++ code.ino
@@ -13,48 +13,68 @@ void setup()
   const byte sensorPin = A0;
 }
 
-void blink () {
+void blink()
+{
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);                         // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);      // turn the LED off by making the voltage LOW
-  delay(1000);                         // wait for a second
+  delay(1000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+  delay(1000);                     // wait for a second
 }
 
-bool wet() {
+bool wet()
+{
   for (int i = 0; i < 10; i++)
   {
     sensorValue = sensorValue + analogRead(sensorPin);
     delay(1);
   }
-  sensorValue = sensorValue/10;
+  sensorValue = sensorValue / 10;
 
-  if (sensorValue > sensitivity) {
+  if (sensorValue > sensitivity)
+  {
     return true;
-  } else {
+  }
+  else
+  {
     return false;
   }
 }
 
 // set value at which alarm starts (between 0 and 506 on input)
-void setSensitivity() {
+void setSensitivity()
+{
   int val = analogRead(sensitivtyPin);
-  sensitivity = val/4;
+  sensitivity = val / 4;
 }
 
+int calcLEDDelay()
+{
+  // above maxSensorValue, the LED blink will not increase
+  int maxSensorVal = 1500;
+  int minBlinkDelay = 50;
+  int maxBlinkDelay = 1000;
+  if (sensorValue > maxSensorVal)
+  {
+    return 50;
+  }
+  else
+  {
+    sensorValue - sensitivity;
+  }
+}
 
 // the loop function runs over and over again forever
 void loop()
 {
   setSensitivity();
 
-  if (wet()) {
+  if (wet())
+  {
     digitalWrite(LED_BUILTIN, HIGH);
   }
 
-
-
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1000);                         // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);      // turn the LED off by making the voltage LOW
-  delay(1000);                         // wait for a second
+  delay(1000);                     // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+  delay(1000);                     // wait for a second
 }
